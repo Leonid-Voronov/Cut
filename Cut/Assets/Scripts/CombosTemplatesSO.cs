24f35cb65bf9c939d3643da5e55f4f6e5723ed99
torn2706 <torn0627@gmail.com>
2023-10-21 17:13:17 +0200

    Combos scriptable objects

---
+++ Cut/Assets/Scripts/CombosTemplatesSO.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cut
+{
+    [CreateAssetMenu(fileName = "CombosTemplates", menuName = "ScriptableObjects/Combos", order = 1)]
+    public class CombosTemplatesSO : ScriptableObject
+    {
+        [SerializeField] private List<Combo> _combos;
+
+        public List<Combo> Combos => _combos;
+        public List<int> GetCombo(int index) { return _combos[index].Value; }
+    }
+
+
+    [System.Serializable]
+    public class Combo
+    {
+        [SerializeField] private List<int> _combo;
+        public List<int> Value => _combo;
+    }
+}
+
