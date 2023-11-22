475f6d93430b904e240d307473ba4bd407a72163
torn2706 <torn0627@gmail.com>
2023-11-22 15:42:05 +0100

    configurated for android build

--- Cut/Assets/Scripts/ComboGenerationFeature/SOClasses/ButtonsHolderSO.cs
+++ Cut/Assets/Scripts/ComboGenerationFeature/SOClasses/ButtonsHolderSO.cs
@@ -6,19 +6,13 @@ namespace Cut
     [CreateAssetMenu(fileName = "Buttons", menuName = "ScriptableObjects/Buttons", order = 2)]
     public class ButtonsHolderSO : ScriptableObject
     {
+        [SerializeField] private List<int> _buttons;
         [SerializeField] private List<int> _leftButtons;
         [SerializeField] private List<int> _rightButtons;
-        private List<int> _buttons;
 
         public List<int> LeftButtons => _leftButtons;
         public List<int> RightButtons => _rightButtons;
         public List<int> Buttons => _buttons;
-
-        private void OnEnable()
-        {
-            _buttons.AddRange(_leftButtons);
-            _buttons.AddRange(_rightButtons);
-        }
     }
 }
 
